0d0d64dfac3e2db564a175aacf380eaf1c8ef403
harris654ui <harris.sheharyar54@gmail.com>
2026-02-23 22:45:40 -0800

    Update Procedure.sql

--- Procedure.sql
+++ Procedure.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE Procedure generate_performance(
+Create or Replace Procedure generate_performance(
 p_date IN DATE
 ) IS
-v_bonus     NUMBER;
-v_bonus_pct  NUMBER;
+v_bonus     Number;
+v_bonus_pct  Number;
 
 BEGIN
 
@@ -16,28 +16,27 @@ Select E.employee_id,
        E.salary,
        P.performance_grade,
        P.review_period
-From HR.Employees E
-Join hr.performance_reviews P
+From hr.employees E
+join hr.performance_reviews P
 on E.employee_id = P.employee_id
 Where p.review_period = p_Date
-)
-LOOP
---Calculate Bonus on respective performance grade
+) LOOP
+--Calculate Bonus on performance grade
 IF EMP.performance_grade = 'A' THEN
-            v_bonus_pct := 0.20;
+v_bonus_pct := 0.20;
 
-     ELSIF emp.performance_grade = 'B' THEN
-            v_bonus_pct := 0.10;   
+ELSIF emp.performance_grade = 'B' THEN
+v_bonus_pct := 0.10;   
 
-        ELSIF emp.performance_grade = 'C' THEN
-            v_bonus_pct := 0.05;   
+ELSIF emp.performance_grade = 'C' THEN
+v_bonus_pct := 0.05;   
 
-        ELSIF emp.performance_grade = 'D' THEN
-            v_bonus_pct := 0.02;   
+ELSIF emp.performance_grade = 'D' THEN
+v_bonus_pct:= 0.02;   
 
-        ELSE  -- 
-            v_bonus_pct := 0;
-        END IF;
+ELSE   
+v_bonus_pct := 0;
+END IF;
 V_bonus := emp.salary * v_bonus_pct;
 
 -- Print Output
